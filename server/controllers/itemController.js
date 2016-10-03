9ca64b5e10937315fd353f4e15f4a173ee8cbfb6
D. Wren <drawnwren@gmail.com>
2016-10-03 15:59:56 -0700

    Un julianed

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -3,49 +3,64 @@ const coinbase = require('coinbase');
 const sendEmail = require('./send-email');
 
 module.exports = {
-  getCategories(req, res) {
+  getCategories(req, res, next) {
     db.items.getAllCategories()
-    .then(result => res.status(200).send(result));
+    .then(result => res.status(200).send(result))
+    .catch(e => next(e));
   },
-  getItem(req, res) {
+  getItem(req, res, next) {
     db.items.getById(req.params.id)
-      .then((result) => {
-        res.json(result[0]);
-      });
+    .then((result) => res.json(result[0]))
+    .catch(e => next(e));
   },
-  buyItem(req, res) {
+  buyItem(req, res, next) {
+    // take the buyer id and the product they're trying to buy from the request
+    const buyer = req.user.user.id;
+    const reqProductId = req.params.id;
+    // declare a variable in the global scope that will hold the item response
+    // from the db
+    let actProduct;
+    // initialize a coinbase client with the buyer's access and refresh token
+    console.log('Look for req.user.accessToken here, ', req.user);
+    const client = new coinbase.Client({
+      accessToken: req.user.accessToken,
+      refreshToken: req.user.refreshToken
+    });
+
     db.items.getById(req.params.id)
     .then((product) => {
-      // Product might be an array probably, so need to [0] need to test it out.
-      db.transactions.updateTransaction(product[0].id, { buyer_id: req.user.user.id })
-      .then(() => {
-        db.items.sold(Number(req.params.id));
-        const client = new coinbase.Client({ accessToken: req.user.accessToken, refreshToken: req.user.refreshToken });
-        const args = {
-          name: `Order for ${product[0].title}`,
-          amount: /* (Number(product[0].price))*/ 0.01,
-          metadata: {
-            customer_id: client.id,
-            customer_name: 'test'
-          },
-          currency: 'USD',
-          type: 'order',
-          style: 'custom_small',
-          success_url: `http://${req.headers.host}/items/${req.params.id}/confirm`,
-          cancel_url: `http://${req.headers.host}/items/${req.params.id}`,
-          customer_defined_amount: false,
-          collect_shipping_address: false,
-          description: `Purchasing: ${product[0].title} on BitBargain`
-        };
-        client.createCheckout(args, (err, checkout) => {
+      actProduct = product[0];
+      return db.transactions.updateTransaction(actProduct.id, { buyer_id: buyer });
+    })
+    .then(() => db.items.sold(Number(req.params.id)))
+    .then(() => {
+      const args = {
+        name: `Order for ${actProduct.title}`,
+        amount: /* (Number(actProduct.price))*/ 0.01,
+        metadata: {
+          customer_id: client.id,
+          customer_name: 'test'
+        },
+        currency: 'USD',
+        type: 'order',
+        style: 'custom_small',
+        success_url: `http://${req.headers.host}/items/${reqProductId}/confirm`,
+        cancel_url: `http://${req.headers.host}/items/${reqProductId}`,
+        customer_defined_amount: false,
+        collect_shipping_address: false,
+        description: `Purchasing: ${actProduct.title} on BitBargain`
+      };
+
+      client.createCheckout(args, (err, checkout) => {
+        if (err) {
           console.log(err, checkout);
-          if (err) {
-            res.json(`Error buying ${product[0].title}`);
-          }
+          next(err);
+        } else {
           res.json(checkout.embed_code);
-        });
+        }
       });
-    });
+    })
+    .catch(e => next(e));
   },
   sellItem(req, res, next) {
     const newItem = req.body;
@@ -115,7 +130,7 @@ module.exports = {
   },
 
   resolveDisputes(req, res) {
-    req.body.polarity; // This is a boolean saying whether someone approved it or not. False means to seller, True means to buyer.
+  //    req.body.polarity; // This is a boolean saying whether someone approved it or not. False means to seller, True means to buyer.
     // We should do something with it
     db.transactions.updateTransaction(req.body.id, {});
   }
